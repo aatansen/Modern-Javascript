84d1905b1ed8ec5c230d1d44eec5033401896bae
Tansen <aatansen@gmail.com>
2022-02-24 21:05:15 +0600

    string

---
+++ Chapter_02_4 - String/sandbox.js
@@ -0,0 +1,27 @@
+// String 
+console.log('Hello World');
+
+// Storing string in a variable
+let email = 'aatansen@gmail.com';
+console.log(email);
+
+// String concatenation
+let firstName = 'Alahi';
+let lastName = 'Tansen';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting single character from string
+console.log(fullName[0]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result,fullName);
+
+let index = email.indexOf('s');
+console.log(index);
+
