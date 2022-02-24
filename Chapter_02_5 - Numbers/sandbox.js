ee9f4ab5a9f428226ea295821fe27d66e9a2e4a5
Tansen <aatansen@gmail.com>
2022-02-25 00:30:47 +0600

    Numbers

---
+++ Chapter_02_5 - Numbers/sandbox.js
@@ -0,0 +1,29 @@
+// let radius = 10;
+// let pi = 3.14;
+// console.log(radius , pi );
+
+// math operators +, -, *, /, **, %
+// let result = radius % 3;
+// console.log(result);
+// let result=pi * radius**2;
+// console.log(result);
+
+// order of operation - B I D M A S
+// let result = 5 * (10 - 3) ** 2;
+// console.log(result);
+
+let likes = 10;
+// likes = likes +1;
+// likes++;
+// likes +=10;
+// likes -=5;
+// likes *=2;
+// likes /=2;
+// console.log(likes);
+
+// NaN - not a number
+// let result = 10 / 'hello';
+// console.log(result);
+
+let result = 'The blog has ' + likes + ' likes';
+console.log(result);
