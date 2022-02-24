ed03adba79614142ea7b69d325c4298e0aea2960
Tansen <aatansen@gmail.com>
2022-02-24 19:40:44 +0600

    variables constants and comments

---
+++ Chapter_02_2/sandbox.js
@@ -0,0 +1,19 @@
+let age = 25;
+let year = 2022;
+console.log(age,year);
+
+// changing age value to 30
+age = 30;
+console.log(age);
+
+const point = 50;
+console.log(point);
+
+// can't change const value
+// point = 40;
+// console.log(point);
+
+// old way of declaring variables
+var score = 75;
+console.log(score);
+
