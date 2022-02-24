2f094511365a659624b0a03a9cdb176a862f0edd
Tansen <aatansen@gmail.com>
2022-02-24 19:59:09 +0600

    Javascipt Data Types

---
+++ Chapter_02_3/sandbox.js
@@ -0,0 +1,22 @@
+// JavaScipt Data Types:
+
+// number 
+let num = 25;
+
+// String 
+let str = "Hello";
+
+// Boolean  
+let bool = true;
+
+// Null 
+let nullVal = null;
+
+// Undefined
+let undef = undefined;
+
+// Object
+// Complex data stuctures - Arrays ,Dates , Literals etc 
+
+// Symbol- used with object
+let sym = Symbol("symbol");
