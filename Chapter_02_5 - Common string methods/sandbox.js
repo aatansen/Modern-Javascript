d1fe901795f6e6c8c90301593725401deeef5e98
Tansen <aatansen@gmail.com>
2022-02-24 23:23:03 +0600

    Common string methods

---
+++ Chapter_02_5 - Common string methods/sandbox.js
@@ -0,0 +1,11 @@
+//common string methods
+let email = 'aatansen@gmail.com';
+// let result = email.lastIndexOf('a');
+
+// let result = email.slice(5,10);
+
+// let result = email.substr(5,10);
+
+// let result = email.replace('a' , 'c');
+
+console.log(result);
