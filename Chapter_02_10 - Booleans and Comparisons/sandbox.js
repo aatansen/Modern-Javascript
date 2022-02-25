d91805d8c4bb80d3a0720db4f6e191ea86357891
Tansen <aatansen@gmail.com>
2022-02-25 10:07:03 +0600

    Booleans and Comparisons

---
+++ Chapter_02_10 - Booleans and Comparisons/sandbox.js
@@ -0,0 +1,24 @@
+// boolean is a data type that can only be true or false
+console.log(true, false);
+
+// methods can return boolean values 
+let email = 'aatansen@gmail.com';
+let result = email.includes('@');
+console.log(result);
+
+// comparisons can be made with booleans
+let age = 25;
+console.log(age == 25);
+console.log(age == 24);
+console.log(age != 24);
+console.log(age > 24);
+console.log(age < 24);
+console.log(age >= 24);
+console.log(age <= 24);
+
+let name = 'Tansen';
+console.log(name == 'Tansen');
+console.log(name == 'tansen');
+
+let name2 = 'zcba';
+console.log(name2 > 'zbca');
