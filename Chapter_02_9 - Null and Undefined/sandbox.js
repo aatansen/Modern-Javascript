3bedb4bcf1edb51ca5b89e8b9bdd87ee0dc018bf
Tansen <aatansen@gmail.com>
2022-02-25 09:54:05 +0600

    Null and Undefined

---
+++ Chapter_02_9 - Null and Undefined/sandbox.js
@@ -0,0 +1,7 @@
+// Undefined is a primitive type that represents the absence of a value.
+let age;
+console.log(age, `The age is ${age}`);
+
+//Nan is a primitive type that represents a value that is not a number.
+let age2=null;
+console.log(age2,age2+2, `The age is ${age2}`);
