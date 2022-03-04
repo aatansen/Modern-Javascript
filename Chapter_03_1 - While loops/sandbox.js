c43c2cfdcb4b39ca0fd5e8dbff24ccf7b102b5f1
Tansen <aatansen@gmail.com>
2022-03-04 17:15:20 +0600

    While loops

---
+++ Chapter_03_1 - While loops/sandbox.js
@@ -0,0 +1,12 @@
+// while loops
+// let i = 0;
+// while (i<10) {
+//     console.log('in loop:',i);
+//     i++;
+// }
+let i = 0;
+const names = ['shaun', 'ryu', 'chun-li'];
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
+}
