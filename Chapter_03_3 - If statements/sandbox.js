d54b5032812714570393c3796a81f597622c8ea6
Tansen <aatansen@gmail.com>
2022-03-04 17:33:04 +0600

    If statements

---
+++ Chapter_03_3 - If statements/sandbox.js
@@ -0,0 +1,15 @@
+// if statements 
+const age = 25;
+if (age > 20){
+    console.log('you are over 20 years old');
+}
+
+const ninjas = ['shaun', 'ryu', 'chun-li'];
+if (ninjas.length > 2){
+    console.log('that is a lot of ninjas');
+}
+
+const password = '12345';
+if (password.length > 4){
+console.log('that is a long password');
+}
