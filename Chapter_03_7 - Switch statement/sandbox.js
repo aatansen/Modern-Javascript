43c2dadcd0a91eabfafae57b3767a7e5c79fbc8e
Tansen <aatansen@gmail.com>
2022-03-04 18:22:33 +0600

    Switch statement

---
+++ Chapter_03_7 - Switch statement/sandbox.js
@@ -0,0 +1,36 @@
+// switch statement
+const grade = 'A';
+switch (grade){
+    case 'A':
+        console.log('You got an A!');
+        break;
+    case 'B':
+        console.log('You got a B!');
+        break;
+    case 'C':
+        console.log('You got a C!');
+        break;
+    case 'D':
+        console.log('You got a D!');
+        break;
+    case 'F':
+        console.log('You got an F!');
+        break;
+    default:
+        console.log('You got an invalid grade!');
+}
+
+//using if statement
+// if (grade === 'A') {
+//     console.log('You got an A!');
+// } else if (grade === 'B') {
+//     console.log('You got an B!');
+// } else if (grade === 'C') {
+//     console.log('You got an C!');
+// } else if (grade === 'D') {
+//     console.log('You got an D!');
+// } else if (grade === 'F') {
+//     console.log('You got an F!');
+// } else {
+//     console.log('You got an invalid grade!');
+// }
