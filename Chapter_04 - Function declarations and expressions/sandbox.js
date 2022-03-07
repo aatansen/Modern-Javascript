ab0738ca6c0c1cb695f9ddbec07e056661610bba
Tansen <aatansen@gmail.com>
2022-03-07 16:13:53 +0600

    Function declarations and expressions

---
+++ Chapter_04 - Function declarations and expressions/sandbox.js
@@ -0,0 +1,11 @@
+// function declaration
+function sayHello() {
+    console.log('say Hello');
+}
+// sayHello();
+
+//function expressons
+const speak = function() {
+    console.log('speak Hello');
+};
+speak();
