26f8b66b42846c23b163a91a7d4fece613238296
Tansen <aatansen@gmail.com>
2022-03-07 16:33:46 +0600

    Returning values

---
+++ Chapter_04_2 - Returning values/sandbox.js
@@ -0,0 +1,16 @@
+// returning values
+
+/*
+const calcArea = function(radius){
+    let area = 3.14 * radius ** 2;
+    return area;
+};
+*/
+
+// cleaning upper code:
+const calcArea = function(radius){
+    return 3.14 * radius ** 2;
+   
+};
+const area=calcArea(5);
+console.log(area);
