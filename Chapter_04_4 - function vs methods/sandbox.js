846942b78651851d37c0efbad854370c9d2073ba
Tansen <aatansen@gmail.com>
2022-03-08 16:12:58 +0600

    function vs method

---
+++ Chapter_04_4 - function vs methods/sandbox.js
@@ -0,0 +1,9 @@
+// functions 
+const greet = ()=> 'hello';
+let result1 = greet();
+console.log(result1);
+
+//methods  
+const name1 = 'Tansen';
+let result2 = name1.toUpperCase()
+console.log(result2);
